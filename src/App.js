af3acb642bee27ee14445f411c095ebe75d53501
drewberry27 <drewberrysteph@gmail.com>
2020-09-27 13:57:46 +0800

    ok keu

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, { useState } from "react";
-import { BrowserRouter, Switch, Route, Redirect, Router } from "react-router-dom";
-import "./App.css";
-import { Layout } from "antd";
-import Dashboard from "./components/pages/Dashboard";
-import Login from "./components/pages/Login";
-import User from "./components/User";
-import Product from "./components/Product";
+import React, { useState } from "react"
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+  Router,
+} from "react-router-dom"
+import "./App.css"
+import { Layout } from "antd"
+import Dashboard from "./components/pages/Dashboard"
+import Login from "./components/pages/Login"
+import User from "./components/User"
+import Product from "./components/Product"
 const App = ({ children }) => {
-  const { Header, Content } = Layout;
+  const { Header, Content } = Layout
 
-  const [loggedInStatus, SetLoggedInStatus] = useState("not_logged_in");
-  const [user, setUser] = useState({});
+  const [loggedInStatus, SetLoggedInStatus] = useState("not_logged_in")
+  const [user, setUser] = useState({})
 
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route
-            exact
-            path="/login"
-            render={(props) => (
-              <Login {...props} loggedInStatus={loggedInStatus} />
-            )}
-          />
-
           {/* {localStorage.getItem("token") ? (
             <Route
               exact
@@ -37,14 +35,19 @@ const App = ({ children }) => {
             <Redirect to="/login" />
           )} */}
 
-          <Route exact path="/users" component={User} />
-          <Route exact path="/" component={Dashboard} />
-
-          <Route exact path="/products" component={Product} />
+          <Route strict path="/" component={Dashboard} />
+          <Route path="/users" component={User} />
+          <Route path="/products" component={Product} />
+          <Route
+            path="/login"
+            render={(props) => (
+              <Login {...props} loggedInStatus={loggedInStatus} />
+            )}
+          />
         </Switch>
       </BrowserRouter>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
